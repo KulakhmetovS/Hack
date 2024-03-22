545478f24609630ea293a09af58b6ce1599cca1e
KulakhmetovS <133137110+KulakhmetovS@users.noreply.github.com>
2024-03-23 02:03:07 +0300

    Created login page v1

--- app/src/components/pages/UserPage.jsx
+++ app/src/components/pages/UserPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import classes from './UserPage.module.css';
 import Header from '../ui/header/Header';
 import Panel from '../ui/panel/Panel';
+import profile_image from '../../images/cat.jpeg';
+import award_image from '../../images/award.jpg';
 
 const UserPage = () => {
 
@@ -13,11 +15,30 @@ const UserPage = () => {
           <div className={classes.content}>
             <div className={classes.profile_container}>
               <div className={classes.profile}>
+                <img src={profile_image}/>
+                <div className={classes.score}><div>Кол-во очков</div></div>
               </div>
               <div className={classes.description}>
+                <div className={classes.userinfo_container}>
+                  <div className={classes.info}>Псевдрним</div>
+                  <div className={classes.field}></div>
+                </div>
+                <div className={classes.userinfo_container}>
+                  <div className={classes.info}>Почта</div>
+                  <div className={classes.field}></div>
+                </div>
+                <div className={classes.raiting}>
+                  <div className={classes.place}>Вы на таком-то месте в рейтинге</div>
+                </div>
               </div>
             </div>
             <div className={classes.awards}>
+              <div className={classes.awards_text}>Стенд достижений</div>
+              <div className={classes.award_images}>
+                <img src={award_image} className={classes.small_image}/>
+                <img src={award_image} className={classes.big_image}/>
+                <img src={award_image} className={classes.small_image}/>
+              </div>
             </div>
           </div>
         </div>
